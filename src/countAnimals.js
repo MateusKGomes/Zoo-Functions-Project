a1513a96dc0b7e8b7c5edf2f2c2d679a07094c0a
Mateus Kulik <mateuskulik30@gmail.com>
2022-11-01 09:35:08 -0300

    Requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  const getSpecie = species.find((specie) => specie.name === animal.specie).residents;
+  if (!animal.sex) {
+    return getSpecie.length;
+  }
+  return getSpecie.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;

f88aba6918587d4f831f917f118507d33a4ee99d
Mateus Kulik <mateuskulik30@gmail.com>
2022-11-03 11:47:39 -0300

    Requisito 9.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,9 +1,34 @@
-// const { species, hours } = require('../data/zoo_data');
-// const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// function getSchedule(scheduleTarget) {
+function getAnimalDay(animal) {
+  return species.find((specie) => specie.name === animal).availability;
+}
 
-// }
-// console.log(getSchedule());
+const animalDay = (weekDay) => species
+  .filter((specie) => specie.availability.includes(weekDay))
+  .map((getName) => getName.name);
 
-// module.exports = getSchedule;
+const objDays = () =>
+  Object.assign({},
+    ...Object.keys(hours).map((weekDays) => {
+      const { open, close } = hours[weekDays];
+      return {
+        [weekDays]: {
+          officeHour: weekDays !== 'Monday' ? `Open from ${open}am until ${close}pm` : 'CLOSED',
+          exhibition: weekDays !== 'Monday' ? animalDay(weekDays) : 'The zoo will be closed!',
+        },
+      };
+    }));
+
+const dayWeek = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+function getSchedule(param) {
+  const verificaDia = dayWeek.includes(param);
+  const verificaAnimal = species.some((specie) => Object.values(specie).includes(param));
+  if (!param || (!verificaDia && !verificaAnimal)) return objDays();
+  if (verificaDia) return { [param]: objDays()[param] };
+  return getAnimalDay(param);
+}
+
+module.exports = getSchedule;
